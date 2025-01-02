805db4562c86c20cca3f47eec76e0b0fb3f088ea
Renan Naves <renan.naves.frontend@gmail.com>
2025-01-02 16:17:04 -0300

    figmaFive

---
+++ meu-portifolio/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/meu-portifolio',
+  server: {
+    open: true, // Opcional: Abre o navegador automaticamente
+  },
+  build: {
+    outDir: 'dist', // Certifique-se de onde os arquivos estáticos são gerados
+  },
+
+})
